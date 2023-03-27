957646faef60c53c29548d50ccedacb3abdf606c
Nigel Omondi <nigelomondi62@gmail.com>
2023-03-27 21:17:27 +0300

    Calculate active flags

--- get_flags.c
+++ get_flags.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include "main.h"
 
 /**
  * get_flags - Calculates active flags
@@ -18,18 +17,15 @@ int get_flags(const char *format, int *i)
 
 	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
 	{
-		for (j = 0; FLAGS_CH[j] != '\0'; j++)
-			if (format[curr_i] == FLAGS_CH[j])
-			{
-				flags |= FLAGS_ARR[j];
-				break;
-			}
-
-		if (FLAGS_CH[j] == 0)
-			break;
+	for (j = 0; FLAGS_CH[j] != '\0'; j++)
+	if (format[curr_i] == FLAGS_CH[j])
+		{
+		flags |= FLAGS_ARR[j];
+		break;
 	}
-
+	if (FLAGS_CH[j] == 0)
+		break;
+}
 	*i = curr_i - 1;
-
 	return (flags);
 }
