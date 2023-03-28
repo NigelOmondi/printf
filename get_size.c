faa7ffed162b14fe3f1be2129d0ede6f563e71b8
ckr25 <ckipkirui208@gmail.com>
2023-03-28 12:02:15 -0700

    get_size.c

--- get_size.c
+++ get_size.c
@@ -2,7 +2,7 @@
 
 /**
  * get_size - calculates the size to cast the argument
- * @format: fornatted string in which to print the argument 
+ * @format: fornatted string in which to print the argument
  * @i: list of arguments to be printed
  *
  * Return: precision
